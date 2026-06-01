1467f20b9b92305a3bbcfe6b7c9fcfcacb2e0fec
jarumitayaa-cmd <jarumitayaa@gmail.com>
2026-06-01 13:19:46 +0530

    Update URL to production environment

--- configs.py
+++ configs.py
@@ -13,7 +13,7 @@ USER_SESSION = os.getenv("USER_SESSION", "") # Use Pyrogram V2 String Session
 
 # Web
 PORT = int(os.getenv("PORT", "8080")) 
-URL = os.getenv("URL", "") # Heroku or Koyeb Or Render Base Url 
+URL = os.getenv("URL", "https://web-production-c02fb5.up.railway.app/") # Heroku or Koyeb Or Render Base Url 
 
 # MongoDB
 DATABASE_URL = os.getenv("DATABASE_URL", "") #Mongodb Url 
